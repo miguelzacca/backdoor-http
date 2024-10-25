9fc279b5ff3ceffc07d77c3a6480934503468f7e
Miguel Zacca <miguel_zacca@estudante.sesisenai.org.br>
2024-10-25 08:17:14 -0300

    fix(general): fix genereal bugs

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -44,10 +44,10 @@ const recipes = document.querySelectorAll('aside button')
 for (const btn of recipes) {
   btn.addEventListener('click', () => {
     const { cmd } = btn.dataset
-    let super_cmd = ''
+    let super_cmd = 'msg * '
 
     if (cmd === 'super_msg') {
-      super_cmd = prompt('Type your message:')
+      super_cmd += prompt('Type your message:')
       if (!super_cmd) {
         return
       }
